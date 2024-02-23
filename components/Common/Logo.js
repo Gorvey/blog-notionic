f6beb351eec3f772aac2f46abbe26715c1897e36
Anjhon <44490800+An-Jhon@users.noreply.github.com>
2024-02-23 15:33:33 +0800

    Update Logo.js

--- components/Common/Logo.js
+++ components/Common/Logo.js
@@ -2,7 +2,7 @@
 import * as React from 'react'
 
 const Logo = (props) => (
-  <img src="https://cdn.jsdelivr.net/gh/An-Jhon/image_bed/202402231430404.png" width="24" height="24">
+  <img src="https://cdn.jsdelivr.net/gh/An-Jhon/image_bed/202402231430404.png" width="24" height="24" />
 )
 
 export default Logo
