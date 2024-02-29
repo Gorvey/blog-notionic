5a970e97161bcd1e612931b46e78b91d5f4c80af
Anjhon <anjhon@163.com>
2024-02-29 16:15:50 +0800

    页脚的 contact 页面隐藏，更换图标

--- components/Common/Social.js
+++ components/Common/Social.js
@@ -14,7 +14,7 @@ const Social = () => {
         aria-label='Mail'
         className='text-gray-400 hover:text-gray-500 active:text-gray-600 transition duration-100'
       >
-        <svg 
+        {/* <svg 
           fill="none" 
           height="24" 
           stroke-width="1.5" 
@@ -24,7 +24,19 @@ const Social = () => {
         >
           <path d="M7 9L12 12.5L17 9" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
           <path d="M2 17V7C2 5.89543 2.89543 5 4 5H20C21.1046 5 22 5.89543 22 7V17C22 18.1046 21.1046 19 20 19H4C2.89543 19 2 18.1046 2 17Z" stroke="currentColor" stroke-width="1.5"/>
+        </svg> */}
+
+        <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          fill="none" 
+          viewBox="0 0 24 24" 
+          stroke-width="1.5" 
+          stroke="currentColor" 
+          class="w-6 h-6"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
         </svg>
+
       </Link>
 
       {/* <Link
